4cc1f1b6bf42056cfd0b49bb7c0492db663eecd0
Ananya Chaurasia <ananyachaurasia74@gmail.com>
2026-06-09 10:56:42 +0530

    [Ananya] add : files added

---
+++ gcr-codebase/control-flow/Syntax.java
@@ -0,0 +1,61 @@
+//if Statement
+//if (condition) {
+    // statements
+//}
+
+//if-else Statement
+//if (condition) {
+    // statements
+//} else {
+    // statements
+//}
+
+//if-else-if Ladder
+//if (condition1) {
+    // statements
+//} else if (condition2) {
+    // statements
+//} else {
+    // statements
+//}
+
+//switch Statement
+//switch (expression) {
+    //case value1:
+        // statements
+        //break;
+    //case value2:
+        // statements
+        //break;
+    // more cases...
+    //default:
+        // statements
+//}
+
+//Nested if Statement
+//if (condition1) {
+    //if (condition2) {
+        // statements
+    //}
+//}
+
+//for Loop
+//for (initialization; condition; update) {
+    // statements
+//}
+
+//while Loop
+//while (condition) {
+    // statements
+//}
+
+//do-while Loop
+//do {
+    // statements
+//} while (condition);
+
+//Enhanced for-each Loop
+//for (type variable : collection) {
+    // statements
+//}
+
